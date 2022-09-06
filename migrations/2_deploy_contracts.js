c7498bff8b0e39cdebcadb8053879273efb48d44
Ocatom-dev <calebomoniyitomiwa@gmail.com>
2022-09-07 00:54:12 +0200

    first

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,8 +6,8 @@ module.exports = function(deployer) {
 
     let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
     deployer.deploy(FlightSuretyData)
-    .then(() => {
-        return deployer.deploy(FlightSuretyApp)
+        .then(() => {
+            return deployer.deploy(FlightSuretyApp)
                 .then(() => {
                     let config = {
                         localhost: {
@@ -16,8 +16,8 @@ module.exports = function(deployer) {
                             appAddress: FlightSuretyApp.address
                         }
                     }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                    fs.writeFileSync(__dirname + '/../src/dapp/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+                    fs.writeFileSync(__dirname + '/../src/server/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
                 });
-    });
+        });
 }
---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
+module.exports = {
+    networks: {
+        development: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+            },
+            network_id: '*',
+            gas: 999999999,
+            skipDryRun: true,
+            gasPrice: 100000000000
+        }
+    },
+    compilers: {
+        solc: {
+            version: "^0.4.24"
+        }
+    }
+};
