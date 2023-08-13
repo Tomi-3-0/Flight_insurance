933bce28c27d3d8392ace5a1eba3ed47f16cecb2
Caleb Omoniyi <81532297+Tomi-3-0@users.noreply.github.com>
2023-08-14 01:22:19 +0200

    Update FlightSuretyApp.sol

--- contracts/FlightSuretyApp.sol
+++ contracts/FlightSuretyApp.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.10;
+pragma solidity >=0.5.10;
 
 // It's important to avoid vulnerabilities due to numeric overflow bugs
 // OpenZeppelin's SafeMath library, when used correctly, protects agains such bugs
